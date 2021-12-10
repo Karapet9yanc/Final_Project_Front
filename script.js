174b1d27f0bb0d6381613d8e5bee9c3181aaeadf
Karapet9yanc <vkarapetiantc.exceedteam@gmail.com>
2021-12-10 18:46:00 +0300

    Code Review

--- script.js
+++ script.js
@@ -1,128 +1,127 @@
 let allPurchases = [];
 let inputWhere = null;
 let inputHow = null;
-let valueInputWhere = "";
-let valueInputHaw = "";
+let valueInputShop = "";
+let valueInputPrice = "";
 let sumBuy = 0;
 
-const day = () => {
-  let today = new Date();
-  const dd = String(today.getDate()).padStart(2, "0");
-  const mm = String(today.getMonth() + 1).padStart(2, "0");
-  const yyyy = today.getFullYear();
-  today = dd + "." + mm + "." + yyyy;
-  return today;
-};
+const day = new Date().toJSON().split("T")[0];
 
 window.onload = async () => {
   inputWhere = document.querySelector("#midll-window");
   inputHow = document.querySelector("#second-window");
-  inputWhere.addEventListener('change', updateValue);
-  inputHow.addEventListener('change', updateValue2);
+  inputWhere.addEventListener("change", updateValueMagazin);
+  inputHow.addEventListener("change", updateValuePrice);
 
-  const resp = await fetch("http://localhost:4040/allPurchases", {
+  const resp = await fetch("http://localhost:8000/allPurchases", {
     method: "GET",
   });
   const result = await resp.json();
   allPurchases = result.data;
   render();
 };
+
 const onclickButtom = async () => {
-  const resp = await fetch("http://localhost:4040/createPurchase", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({
-      text: valueInputWhere,
-      text2: valueInputHaw,
-      date: day(),
-    }),
-  });
-  const result = await resp.json();
-  allPurchases.push(result.data);
+  if (valueInputShop && valueInputPrice) {
+    const resp = await fetch("http://localhost:8000/createPurchase", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        text: valueInputShop,
+        text2: valueInputPrice,
+        date: day,
+      }),
+    });
+    const result = await resp.json();
+    allPurchases.push(result.data);
 
-  valueInputHaw = '';
-  valueInputWhere = '';
-  inputHow.value = '';
-  inputWhere.value = '';
-  render();
-}
-
-const updateValue = (event) => {
-  valueInputWhere = event.target.value;
+    valueInputPrice = "";
+    valueInputShop = "";
+    inputHow.value = "";
+    inputWhere.value = "";
+    render();
+  } else {
+    alert("Введите в пустое поле значение");
+  }
 };
-const updateValue2 = (event) => {
-  valueInputHaw = event.target.value;
+
+const updateValueMagazin = (event) => {
+  valueInputShop = event.target.value;
+};
+const updateValuePrice = (event) => {
+  valueInputPrice = event.target.value;
 };
 
 const validate = (evt) => {
-  let theEvent = evt || window.event;
+  const theEvent = evt || window.event;
   let onlyNum = theEvent.onlyNumCode || theEvent.which;
   onlyNum = String.fromCharCode(onlyNum);
-  let regex = /[0-9]|\./;
+  const regex = /[0-9]|\./;
   if (!regex.test(onlyNum)) {
     theEvent.returnValue = false;
-    if (theEvent.preventDefault) theEvent.preventDefault()
-  };
+    if (theEvent.preventDefault) theEvent.preventDefault();
+  }
 };
 
-render = () => {
+const render = () => {
   const expenses = document.getElementById("shopping");
   while (expenses.firstChild) {
-    expenses.removeChild(expenses.firstChild)
-  };
+    expenses.removeChild(expenses.firstChild);
+  }
   allPurchases.map((item, index) => {
-    const container = document.createElement('div');
-    container.className = 'allPurchases-container';
+    const container = document.createElement("div");
+    container.className = "allPurchases-container";
     container.id = `task-${index}`;
-    const block1 = document.createElement('div');
+    const block1 = document.createElement("div");
     block1.id = `first`;
-    const one = document.createElement('div');
+    const one = document.createElement("div");
     one.id = `clone`;
-    const two = document.createElement('div');
+    const two = document.createElement("div");
     two.id = `result`;
-    const nameShop = document.createElement('p');
+    const nameShop = document.createElement("p");
     nameShop.innerText = `${index + 1}) Магазин :`;
-    const text = document.createElement('p');
+    const text = document.createElement("p");
+    text.id = `elem`;
     text.innerText = item.text;
     one.appendChild(nameShop);
     two.appendChild(text);
     block1.appendChild(one);
     block1.appendChild(two);
     expenses.appendChild(block1);
-    const blockData = document.createElement('div');
-    blockData.id = 'data1';
-    const date = document.createElement('p');
-    date.className = 'date';
+    const blockData = document.createElement("div");
+    blockData.id = "data1";
+    const date = document.createElement("p");
+    date.className = "date";
     date.innerText = item.date;
     blockData.appendChild(date);
-    const block2 = document.createElement('div');
-    block2.id = 'second';
-    const how = document.createElement('p');
+    const block2 = document.createElement("div");
+    block2.id = "second";
+    const how = document.createElement("p");
     how.innerText = item.text2;
-    const verstka = document.createElement('div');
-    verstka.id = 'verstka';
-    const Rub = document.createElement('span');
-    Rub.innerText = ' р.';
+    const verstka = document.createElement("div");
+    verstka.id = "verstka";
+    const Rub = document.createElement("span");
+    Rub.innerText = " р.";
     how.appendChild(Rub);
     block2.appendChild(how);
     sumBuy = sumBuy + Number(item.text2);
-    const imageClick = document.createElement('div');
-    imageClick.id = 'imageClick';
-    const imageSave = document.createElement('img');
-    imageSave.src = 'img/save.png';
-    const imageEdit = document.createElement('img');
-    imageEdit.src = 'img/edit.png';
-    const inpSave = document.createElement('input');
-    inpSave.id = 'ret';
-    const inpReplace = document.createElement('input');
-    inpReplace.type = 'number';
-    inpReplace.id = 'ret';
-    const inpData = document.createElement('input');
-    inpData.type = 'date';
-    inpData.id = 'ret';
+    const imageClick = document.createElement("div");
+    imageClick.id = "imageClick";
+    const imageSave = document.createElement("img");
+    imageSave.src = "img/save.png";
+    const imageEdit = document.createElement("img");
+    imageEdit.src = "img/edit.png";
+    const inpSave = document.createElement("input");
+    inpSave.id = "ret";
+    const inpReplace = document.createElement("input");
+    inpReplace.type = "number";
+    inpReplace.id = "ret";
+    const inpData = document.createElement("input");
+    inpData.type = "date";
+    inpData.id = "ret";
     imageEdit.onclick = () => {
       two.replaceChild(inpSave, text);
       blockData.replaceChild(inpData, date);
@@ -133,31 +132,37 @@ render = () => {
       inpData.value = item.date;
     };
     imageSave.onclick = async () => {
-      imageClick.replaceChild(imageEdit, imageSave);
-      two.replaceChild(text, inpSave);
-      block2.replaceChild(how, inpReplace);
-      text.innerText = inpSave.value;
-      how.innerText = inpReplace.value;
-      item.date = inpData.value.split('-').reverse().join('.');
-      item.text = inpSave.value;
-      item.text2 = inpReplace.value;
-      const resp = await fetch("http://localhost:4040/updatePurchase", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-          "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify(...allPurchases)
-      });
-      const result = await resp.json();
-      allPurchases = result.data;
-      render();
+      if (
+        (item.text = inpSave.value.trim()) &&
+        (item.text2 = inpReplace.value)
+      ) {
+        two.replaceChild(text, inpSave);
+        block2.replaceChild(how, inpReplace);
+        text.innerText = inpSave.value;
+        how.innerText = inpReplace.value;
+        item.date = inpData.value;
+        item.text = inpSave.value;
+        item.text2 = inpReplace.value;
+        const resp = await fetch("http://localhost:8000/updatePurchase", {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify(...allPurchases),
+        });
+        const result = await resp.json();
+        allPurchases = result.data;
+        render();
+      } else {
+        alert("Заполните пустоле поле!!!");
+      }
     };
-    imageClick.appendChild(imageEdit)
-    const purchase = document.createElement('div')
-    purchase.id = 'purchase'
-    const imageDelete = document.createElement('img');
-    imageDelete.src = 'img/delete.png';
+    imageClick.appendChild(imageEdit);
+    const purchase = document.createElement("div");
+    purchase.id = "purchase";
+    const imageDelete = document.createElement("img");
+    imageDelete.src = "img/delete.png";
     imageDelete.onclick = () => {
       expenses.removeChild(purchase);
       onDeleteallPurchases(index);
@@ -173,20 +178,23 @@ render = () => {
     verstka.appendChild(block2);
     verstka.appendChild(imageClick);
     expenses.appendChild(purchase);
-  })
+  });
   const byend = document.getElementById("byend");
   byend.innerText = sumBuy;
   sumBuy = 0;
-}
+};
 const onDeleteallPurchases = async (index) => {
-  const resp = await fetch(`http://localhost:4040/deletePurchase?_id=${allPurchases[index]._id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "text/html; charset=utf-8",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  const resp = await fetch(
+    `http://localhost:8000/deletePurchase?_id=${allPurchases[index]._id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "text/html; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
   const result = await resp.json();
   allPurchases = result.data;
   render();
-}
+};
